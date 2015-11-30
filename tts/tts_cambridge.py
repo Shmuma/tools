9ce63e584654006ace3deeb9b5f9ffab0220158f
Max Lapan <max.lapan@gmail.com>
2015-11-30 21:41:26 +0100

    Cambridge mp3

---
+++ tts/tts_cambridge.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Cambridge pronunciation extractor
+"""
+import argparse
+import urllib2
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser()
+parser.add_argument("word", type=str, help="Word to save pronunciation")
+
+args = parser.parse_args()
+print args
+
+URL = "http://dictionary.cambridge.org/pronunciation/english/%s" % args.word
+
+page = urllib2.urlopen(URL).read()
+
+soup = BeautifulSoup(page, "html.parser")
+mp3_url = None
+for div in soup.find_all("div"):
+    mp3 = div.get('data-src-mp3')
+    if mp3 is not None and mp3.find("uk") >= 0:
+        mp3_url = mp3
+
+uk_div = soup.find('div', {'class': 'big-pron-uk-container'})
+print uk_div.find('span', {'class': 'ipa'}).get_text()
+
+file_name = "%s.mp3" % args.word
+
+with open(file_name, "w+") as fd:
+    mp3_data = urllib2.urlopen(mp3_url).read()
+    fd.write(mp3_data)
+
+print "MP3 saved in " + file_name
+
+
